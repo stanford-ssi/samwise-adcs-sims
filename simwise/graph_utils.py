4429921e11c6f835ac1eb2f4c84cd06d36c60303
Flynn Dreilinger <flynnd@stanford.edu>
2024-11-07 20:04:18 -0800

    Co-authored by: - @arunchandler - @sidanantha - @acarlham - @tycho-naut

--- simwise/graph_utils.py
+++ simwise/graph_utils.py
@@ -61,21 +61,21 @@ def graph_euler(t_arr, y):
         x=t_arr, y=y[:, 0],
         name='phi',
         mode='markers',
-        marker_color='rgba(152, 0, 0, .1)'
+        marker_color='rgba(152, 0, 0, 0.8)'
     ), row=1, col=1)
 
     fig.append_trace(go.Scatter(
         x=t_arr, y=y[:, 1],
         name='theta',
         mode='markers',
-        marker_color='rgba(152, 0, 0, .1)'
+        marker_color='rgba(152, 0, 0, 0.8)'
     ), row=2, col=1)
 
     fig.append_trace(go.Scatter(
         x=t_arr, y=y[:, 2],
         name='psi',
         mode='markers',
-        marker_color='rgba(152, 0, 0, .1)'
+        marker_color='rgba(152, 0, 0, 0.8)'
     ), row=3, col=1)
 
     fig.show()
@@ -86,29 +86,68 @@ def graph_quaternion(t_arr, y):
         x=t_arr, y=y[:, 0],
         name='w',
         mode='markers',
-        marker_color='rgba(152, 0, 0, .1)'
+        marker_color='rgba(152, 0, 0, 0.8)'
     ), row=1, col=1)
 
     fig.append_trace(go.Scatter(
         x=t_arr, y=y[:, 1],
         name='x',
         mode='markers',
-        marker_color='rgba(152, 0, 0, .1)'
+        marker_color='rgba(152, 0, 0, 0.8)'
     ), row=2, col=1)
 
     fig.append_trace(go.Scatter(
         x=t_arr, y=y[:, 2],
         name='y',
         mode='markers',
-        marker_color='rgba(152, 0, 0, .1)'
+        marker_color='rgba(152, 0, 0, 0.8)'
     ), row=3, col=1)
 
     fig.append_trace(go.Scatter(
         x=t_arr, y=y[:, 3],
         name='z',
         mode='markers',
-        marker_color='rgba(152, 0, 0, .1)'
+        marker_color='rgba(152, 0, 0, 0.8)'
     ), row=4, col=1)
 
     fig.show()
 
+def graph_orbital_elements(t_arr, y):
+    fig = make_subplots(rows=6, cols=1)
+    fig.append_trace(go.Scatter(
+        x=t_arr, y=y[:, 0],
+        name='a',
+        mode='markers',
+        marker_color='rgba(152, 0, 0, 0.8)'
+    ), row=1, col=1)
+    fig.append_trace(go.Scatter(
+        x=t_arr, y=y[:, 1],
+        name='e',
+        mode='markers',
+        marker_color='rgba(152, 0, 0, 0.8)'
+    ), row=2, col=1)
+    fig.append_trace(go.Scatter(
+        x=t_arr, y=y[:, 2],
+        name='i',
+        mode='markers',
+        marker_color='rgba(152, 0, 0, 0.8)'
+    ), row=3, col=1)
+    fig.append_trace(go.Scatter(
+        x=t_arr, y=y[:, 3],
+        name='Ω',
+        mode='markers',
+        marker_color='rgba(152, 0, 0, 0.8)'
+    ), row=4, col=1)
+    fig.append_trace(go.Scatter(
+        x=t_arr, y=y[:, 4],
+        name='ω',
+        mode='markers',
+        marker_color='rgba(152, 0, 0, 0.8)'
+    ), row=5, col=1)
+    fig.append_trace(go.Scatter(
+        x=t_arr, y=y[:, 5],
+        name='θ',
+        mode='markers',
+        marker_color='rgba(152, 0, 0, 0.8)'
+    ), row=6, col=1)
+    fig.show()
